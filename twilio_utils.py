0f67a04175f46d2b0a607f23f56f5428fc820981
ANIKET PRASHANT KAKDE <103921626+AniketKakde04@users.noreply.github.com>
2025-06-30 11:09:39 +0530

    Update twilio_utils.py

--- twilio_utils.py
+++ twilio_utils.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
 def download_audio_file(audio_url: str) -> bytes:
-    # Twilio sends media as temporary public URL
-    response = requests.get(audio_url)
+    auth = (os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+    response = requests.get(audio_url, auth=auth)
     response.raise_for_status()
     return response.content
